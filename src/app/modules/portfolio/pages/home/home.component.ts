b84f03879b32a6cf3aca44757cf8564b7f03ecc8
MarcosWinther <winthermarcos@gmail.com>
2023-12-27 00:02:50 -0300

    Header Component parte 1

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [HeaderComponent]
 })
 export class HomeComponent {
 
