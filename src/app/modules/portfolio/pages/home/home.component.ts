66c0007416b7e2acc9027285465eb870db9bdee6
MarcosWinther <winthermarcos@gmail.com>
2023-12-27 23:42:14 -0300

    Knpwledge component parte 1

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, KnowledgeComponent]
 })
 export class HomeComponent {
 
